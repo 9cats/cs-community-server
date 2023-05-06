36f5fd419db70e128d0c6470715606fc99fcb99a
9cats <123337671@qq.com>
2023-05-06 19:40:18 +0800

    chore: bump version

---
+++ src/script/getCommunity.ts
@@ -0,0 +1,19 @@
+import { CSCore, CS } from "../";
+
+const keys = Object.keys(CS.community) as CSCore.Community.Name[];
+const json = {};
+for (const key of keys) {
+  const value = CS.community[key];
+  json[key] = {
+    name: value.name,
+    label: value.label,
+    servers: value.servers.map((server) => ({
+      name: server.name,
+      mode: server.mode,
+      host: server.host,
+      port: server.port,
+    })),
+  };
+}
+
+console.log(JSON.stringify(json));
